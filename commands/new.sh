e5fc534ab0f8f5492605f0ceaebeb5c82911f9c2
Sanket Jain <sanketjain07032000@gmail.com>
2025-03-14 23:51:00 +0530

    Added command script for 'new' command.

---
+++ commands/new.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Base Directory - The directory that contains all files for the CLI tool
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Sourcing utils.sh for utilities
+source "$BASE_DIR/../lib/utils.sh"
+
+# Commands Directory - The directory that contains all supported commands of the CLI tool
+COMMANDS_DIR="$BASE_DIR/../commands"
+
+SUPPORTED_TECH_FILE="$(dirname "${BASH_SOURCE[0]}")/../config/supported_tech"
+
+TECHNOLOGY_OPTION=$1
+
+# If no TECHNOLOGY OPTION is provided, show CLI version and help menu
+if [ -z "$TECHNOLOGY_OPTION" ]; then
+    # Show CLI version and prompt to navigate help menu for "new" command
+    sh "$COMMANDS_DIR/version.sh"
+    print "Run 'initx help new' for a list of available options."
+    exit 1
+fi
+
+# Check if the technology exists in supported_tech
+if ! grep -Fxq "$TECHNOLOGY_OPTION" "$SUPPORTED_TECH_FILE"; then
+    print "'$TECHNOLOGY_OPTION' is not a supported technology." "ERROR"
+    print "Run 'initx list tech' to see supported technologies."
+    exit 1
+fi
+
+case "$TECHNOLOGY_OPTION" in
+    # node
+esac
